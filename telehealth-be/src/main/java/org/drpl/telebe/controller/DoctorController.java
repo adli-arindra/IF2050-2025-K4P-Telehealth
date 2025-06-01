2254b2bdc38d2228f75fbeb9a19d3ff8a8a908ad
firayasmin <firayasmin8@gmail.com>
2025-06-01 23:27:29 +0700

    feat : implementing DoctorController.java

--- telehealth-be/src/main/java/org/drpl/telebe/controller/DoctorController.java
+++ telehealth-be/src/main/java/org/drpl/telebe/controller/DoctorController.java
@@ -1,32 +1,26 @@
 package org.drpl.telebe.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.drpl.telebe.model.Doctor;
+import org.drpl.telebe.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/doctors")
+@RequestMapping("/doctors")
 public class DoctorController {
 
-    @GetMapping
-    public ResponseEntity<String> getAllDoctors() {
-        System.out.println("Get all doctors request received");
-        return ResponseEntity.ok("List of doctors (placeholder)");
-    }
+    @Autowired
+    private DoctorRepository doctorRepository;
 
-    @GetMapping("/{doctorId}")
-    public ResponseEntity<String> getDoctorById(@PathVariable String doctorId) {
-        System.out.println("Get doctor by ID " + doctorId + " request received");
-        return ResponseEntity.ok("Doctor details for " + doctorId + " (placeholder)");
+    @GetMapping
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
     }
 
     @PostMapping
-    public ResponseEntity<String> createDoctor(@RequestBody String doctorRequest) {
-        System.out.println("Create doctor request received: " + doctorRequest);
-        return ResponseEntity.ok("Doctor created (placeholder)");
+    public Doctor createDoctor(@RequestBody Doctor doctor) {
+        return doctorRepository.save(doctor);
     }
 }
