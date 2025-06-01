8d4ce847f4419338451c53c7ebaf0305fbbdb0f4
firayasmin <firayasmin8@gmail.com>
2025-06-01 20:42:54 +0700

    Doctor.java

--- telehealth-be/src/main/java/org/drpl/telebe/model/Doctor.java
+++ telehealth-be/src/main/java/org/drpl/telebe/model/Doctor.java
@@ -1,4 +1,31 @@
 package org.drpl.telebe.model;
 
 public class Doctor {
+    private String specialization;
+    private String license_number;
+
+    public Doctor() {
+    }
+
+    public Doctor(String specialization, String license_number) {
+        this.specialization = specialization;
+        this.license_number = license_number;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getLicense_number() {
+        return license_number;
+    }
+
+    public void setLicense_number(String license_number) {
+        this.license_number = license_number;
+    }
 }
+
