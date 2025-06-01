0610da58e4240fe56ddba6aecb2a4ddf16d41852
Adli Arindra <adliarindra27@gmail.com>
2025-06-01 14:04:36 +0700

    feat: implemented model.ChatSession

--- telehealth-be/src/main/java/org/drpl/telebe/model/ChatSession.java
+++ telehealth-be/src/main/java/org/drpl/telebe/model/ChatSession.java
@@ -1,4 +1,42 @@
 package org.drpl.telebe.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatSession {
+    private String sessionId;
+    private final List<ChatMessage> messages;
+
+    public ChatSession(String sessionId) {
+        this.sessionId = sessionId;
+        this.messages = new ArrayList<>();
+    }
+
+    public List<ChatMessage> getMessages() {
+        return messages;
+    }
+
+    public void addMessage(ChatMessage newMessage) {
+        this.messages.add(newMessage);
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String toJson() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
