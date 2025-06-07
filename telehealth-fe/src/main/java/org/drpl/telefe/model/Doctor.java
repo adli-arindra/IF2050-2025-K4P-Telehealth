af7396a926c178ff47619771bddc7a6098c88cc2
ridofinovalda <ridofinovalda888@gmail.com>
2025-06-07 23:17:58 +0700

    feat: implemented model.Doctor and fetcher.DoctorFetcher

--- telehealth-fe/src/main/java/org/drpl/telefe/model/Doctor.java
+++ telehealth-fe/src/main/java/org/drpl/telefe/model/Doctor.java
@@ -1,4 +1,63 @@
 package org.drpl.telefe.model;
 
+import java.time.LocalDate;
+
 public class Doctor {
+    private Long id;
+    private String name;
+    private String email;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String specialization;
+
+    public Doctor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public LocalDate getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(LocalDate tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
 }
