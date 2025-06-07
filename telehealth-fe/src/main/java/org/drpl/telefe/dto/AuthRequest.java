3da6480eeba94630eb8b33da41c293583bda98dd
firayasmin <firayasmin8@gmail.com>
2025-06-07 23:11:12 +0700

    feat: implemented model.User and fetcher.AuthFetcher

---
+++ telehealth-fe/src/main/java/org/drpl/telefe/dto/AuthRequest.java
@@ -0,0 +1,31 @@
+package org.drpl.telefe.dto;
+
+public class AuthRequest {
+    private String email;
+    private String password;
+
+    public AuthRequest() {
+    }
+
+    public AuthRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and setters
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String GetPassword() {
+        return this.password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
